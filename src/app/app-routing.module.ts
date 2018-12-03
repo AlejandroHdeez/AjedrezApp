84df84934a466b7f19e7abceb600d83dbd513f48
AlejandroHdeez <jaandrohernandez@gmail.com>
2018-12-03 15:45:51 -0600

    MenuPrincipal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { JuegoComponent } from './componentes/juego/juego.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'inicio', component: InicioComponent},
+  {path: 'juego', component: JuegoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { JuegoComponent } from './componentes/juego/juego.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    InicioComponent,
+    JuegoComponent
   ],
   imports: [
     BrowserModule,
