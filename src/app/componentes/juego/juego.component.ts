4d6fbd22ba2578b43be5689c2f08bae547879985
AlejandroHdeez <jaandrohernandez@gmail.com>
2018-12-04 15:51:46 -0600

    Maquetacion de Tablero

--- src/app/componentes/juego/juego.component.ts
+++ src/app/componentes/juego/juego.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Identifiers } from '@angular/compiler';
 
 @Component({
   selector: 'app-juego',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JuegoComponent implements OnInit {
 
+  peon: number = 1;
+  moverPeon: number;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  Movimiento(){
+    this.moverPeon = this.peon
+    this.peon=null;
   }
 
 }
